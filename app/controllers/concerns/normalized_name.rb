1463073c8d36e1831fece5f7200cd3f19266cac2
Peter Cormack <petertcormack@gmail.com>
2023-06-06 20:07:42 -0700

    use a concern to DRY

---
+++ app/controllers/concerns/normalized_name.rb
@@ -0,0 +1,17 @@
+module NormalizedName extend ActiveSupport::Concern
+  included do
+    before_validation :normalize_name
+
+    validates :name, presence: true
+
+    def to_s
+      name.titleize
+    end
+
+    private
+
+    def normalize_name
+      self.name = name.to_s.downcase
+    end
+  end
+end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,15 +1,3 @@
 class Category < ApplicationRecord
-  before_validation :normalize_name
-
-  validates :name, presence: true
-
-  def to_s
-    name.titleize
-  end
-
-  private
-
-  def normalize_name
-    self.name = name.to_s.downcase
-  end
+  include NormalizedName
 end
